c6411b2acf9e2c41b63a921ff2a474139ebfcfde
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2023-12-06 14:15:09 +0100

    Update Annonces.php

--- lesupercoin/src/Entity/Annonces.php
+++ lesupercoin/src/Entity/Annonces.php
@@ -5,7 +5,6 @@ namespace App\Entity;
 use App\Repository\AnnoncesRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-
 #[ORM\Entity(repositoryClass: AnnoncesRepository::class)]
 class Annonces
 {
